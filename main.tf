60cc7c1fd765ad5b2bfdc6e2c98faf8fc905e05a
Joey Lorich <joseph@Lorich.me>
2019-09-13 13:01:19 -0600

    remove vars, add location

--- main.tf
+++ main.tf
@@ -5,12 +5,11 @@ resource "azurerm_resource_group" "default" {
 
 resource "azurerm_container_group" "ghost" {
   name                = "ghost"
-  location            = var.loc
+  location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_name_label      = "mtcden-test-tfcloud"
   ip_address_type     = "public"
   os_type             = "linux"
-  tags                = var.tags
 
   container {
     name   = "ghost-blog"
